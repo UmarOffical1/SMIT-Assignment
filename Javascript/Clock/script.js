39ad7c395dc3d1081560eb7a7ff8f4d50d41ae78
Muhammad Umar Raza <umar@1092007@gmail.com>
2026-02-01 00:12:36 +0500

    done

--- Javascript/Clock/script.js
+++ Javascript/Clock/script.js
@@ -1,93 +1,42 @@
-var dateObject = new Date();
-
-
-var currentDay = dateObject.getDay(); // 2
-var currentMonth = dateObject.getMonth();
-var miliSec = dateObject.getMilliseconds();
-var time = dateObject.getTime();
-var hours = 23;  // 0-23
-
-
-// console.log("date object: " + dateObject)
-// console.log(currentDay)
-// console.log(miliSec)
-// console.log(time)
-
-
-// var days = ['Sun', 'Mon', 'Tue', 'Wed','Thur', 'Fri', 'Sat' ];
-
-// document.write(days[currentDay]);
-// console.log(hours);
-
-
-//am
-// 0 - 11 
-// hours += 1;
-
-// if(hours == 0){
-//     console.log(12 + 'am')
-// }
-// else if(hours >=1 && hours <= 11 ){ 
-//     console.log((hours) +'am')
-// }
-// else if(hours == 12){
-//     console.log(12+'pm');
-// }
-// else{  
-//     hours -= 12;
-//     console.log(hours+'pm');
-// }
-
-
-
-
-/// specific date
-// var futureDate = new Date("January 21, 2030, 15:30:59");
-// console.log(futureDate)
-
-// var currentMilliSec = dateObject.getTime()/// millisec from 1 Jan 1970 till now
-// var futureMilliSec = futureDate.getTime()/// millisec from 1 Jan 1970 till 2030
-
-// var diffInMilliSec = futureMilliSec - currentMilliSec;
-
-// console.log('differnce: '+ diffInMilliSec);
-
-// console.log(Math.floor(diffInMilliSec / (1000 * 60 * 60 * 24 * 30 * 12))) /// s -> min -> hr -> day -> month -> year
-
-
-
-
-
-// digital clock logic
+var hrSpan = document.getElementById('hours');
 var minSpan = document.getElementById('min');
 var secSpan = document.getElementById('sec');
-var hrspn = document.getElementById('hours')
+var ampmSpan = document.getElementById('ampm');
+var dateDiv = document.getElementById('date');
 
-function getCurrentTime(){
-    var currentTime = new Date();
+function getCurrentTime() {
+  var now = new Date();
 
-    if( currentTime.getMinutes() < 10){
-        minSpan.innerText = '0'+currentTime.getMinutes();
-        hrspn.innerText = currentTime.getHours()
-        
-    }
-    else{
-        minSpan.innerText = currentTime.getMinutes();
-        hrspn.innerText = currentTime.getHours()
-    }
-    
-    if(currentTime.getSeconds() < 10){
-        
-        hrspn.innerText = currentTime.getHours()
-        secSpan.innerText = '0'+currentTime.getSeconds();
-    }
-    else{
-        hrspn.innerText = currentTime.getHours()
-        secSpan.innerText = currentTime.getSeconds();
-    }
+  // ---- TIME ----
+  var hours = now.getHours(); // 0–23
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
 
+  // convert 24h to 12h
+  hours = hours % 12;
+  hours = hours ? hours : 12; // 0 → 12
 
+  // leading zero
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  hrSpan.innerText = hours;
+  minSpan.innerText = minutes;
+  secSpan.innerText = seconds;
+  ampmSpan.innerText = ampm;
+
+  // ---- DATE ----
+  var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+  var dayName = days[now.getDay()];
+  var monthName = months[now.getMonth()];
+  var date = now.getDate();
+  var year = now.getFullYear();
+
+  dateDiv.innerText = `${dayName}, ${date} ${monthName} ${year}`;
 }
 
-
-setInterval( getCurrentTime , 1000 ) 
+getCurrentTime(); // run once immediately
+setInterval(getCurrentTime, 1000);
