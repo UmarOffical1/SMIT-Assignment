2dcc01157164b742556252b7757832fd070299e5
Muhammad Umar Developer <umar1292007@gmail.com>
2026-02-03 18:48:31 +0500

    done

--- Javascript/Clock/script.js
+++ Javascript/Clock/script.js
@@ -18,6 +18,7 @@ function getCurrentTime() {
   hours = hours ? hours : 12; // 0 → 12
 
   // leading zero
+  hours = hours < 10 ? '0' + hours : hours;
   minutes = minutes < 10 ? '0' + minutes : minutes;
   seconds = seconds < 10 ? '0' + seconds : seconds;
 
